aa363863a3b788191bfae239c90f84df5641b18e
twined quartz <fmorisato@gmail.com>
2019-05-14 22:19:17 +0900

    Output of regression model

--- fumipo_stat/regression.py
+++ fumipo_stat/regression.py
@@ -7,6 +7,12 @@ importr("glm2")
 from functools import reduce
 
 
+def presentable(func):
+    def wrapper(self, presenter=lambda d: d):
+        return presenter(func(self))
+    return wrapper
+
+
 def get_variable_names_of_R_poly(express: str):
     """
     get variable names of polynominal form expressed as R syntax
@@ -46,19 +52,31 @@ class IRegressionModelResult:
     def __repr__(self):
         return f"{self.model_discription}\n"\
             + f"Objective: {self.objective_name}\n"\
-            + f"Variables: {self.variable_names}\n"
+            + f"Variables: {self.get_variables()}\n"
 
     def result(self):
         return self.model_result
 
-    def summary(self):
+    def get_summary_section(self, index, default=None):
+        try:
+            return self.summary()[:][index]
+        except:
+            return default
+
+    def summary(self, default=None):
         return base.summary(self.result())
 
     def y(self):
         return self.objective_name
 
-    def variables(self):
-        return self.variable_names
+    def get_variables(self):
+        pass
+
+    def colnames(self, index):
+        return list(self.summary()[:][index].colnames)
+
+    def rownames(self, index):
+        return list(self.summary()[:][index].names)
 
 
 class IRegressionModel:
@@ -76,9 +94,6 @@ class IRegressionModel:
 
     def discript_model(self)->str:
         pass
-
-    def result(self):
-        return self._result
 
 
 class FailedResult(IRegressionModelResult):
@@ -108,35 +123,102 @@ class LMResult(IRegressionModelResult):
                                        objective_variable_name,
                                        model_discription,)
         self.model_type = "R built in lm"
-        self.get_variables()
 
-    def get_coeff_matrix(self,coeff_index):
-        return self.summary()[:][coeff_index]
+    def get_variables(self):
+        coeff_matrix = self.get_summary_section(3, None)
+        variables = list(map(get_variable_names_of_R_poly,
+                             coeff_matrix.names[0])) if coeff_matrix is not None else []
+        return variables
 
-    def get_variables(self,coeff_index):
-        names = list(map(get_variable_names_of_R_poly,
-                         self.get_coeff_matrix(coeff_index).names[0]))
-        self.variable_names = names
+    @presentable
+    def coeff(self):
+
+        coeff_matrix = self.get_summary_section(3, None)
+
+        if coeff_matrix is None:
+            return (PrettyNamedMatrix([[None for __ in range(4)] for _ in range(1)], colnames=range(4), rownames=range(1)))
+        else:
+            variables = self.get_variables()
+            return (PrettyNamedMatrix(coeff_matrix, rownames=variables))
+
+    @presentable
+    def r_sqared(self):
+        r2 = self.get_summary_section(8, None)
+        return r2[0] if r2 is not None else None
 
 
-class GLMResult(IRegressionModelResult):
+class GLM2Result(IRegressionModelResult):
     def __init__(self,
                  model_result,
                  objective_variable_name,
                  model_discription,
                  ):
-        super(GLMResult, self).__init__(model_result,
-                                        objective_variable_name,
-                                        model_discription,)
+        super(GLM2Result, self).__init__(model_result,
+                                         objective_variable_name,
+                                         model_discription,)
         self.model_type = "R package glm2"
-        self.get_variables()
-
-    def get_coeff_matrix(self):
-        return self.summary()[:][11]
 
     def get_variables(self):
-        names = list(map(get_variable_names_of_R_poly,self.get_coeff_matrix().names[0]))
-        self.variable_names = names
+        coeff_matrix = self.get_summary_section(11, None)
+        names = list(map(get_variable_names_of_R_poly,
+                         coeff_matrix.names[0])) if coeff_matrix is not None else []
+        return names
+
+    @presentable
+    def coeff(self):
+        coeff_matrix = self.get_summary_section(11, None)
+
+        if coeff_matrix is None:
+            return (PrettyNamedMatrix([[None for __ in range(4)] for _ in range(1)], colnames=range(4), rownames=range(1)))
+        else:
+            variables = self.get_variables()
+            return (PrettyNamedMatrix(coeff_matrix, rownames=variables))
+
+    @presentable
+    def AIC(self):
+        if type(self.result()) is FailedResult:
+            return (None)
+        aic = self.get_summary_section(4)[0]
+        return (aic)
+
+    @presentable
+    def residual_deviance(self):
+        res_dev = self.get_summary_section(3)
+        return res_dev[0] if res_dev is not None else None
+
+    @presentable
+    def null_deviance(self):
+        null_dev = self.get_summary_section(7)
+        return null_dev[0] if null_dev is not None else None
+
+    @presentable
+    def psuedo_r_sqared(self):
+        return (1-self.residual_deviance()/self.null_deviance())
+
+
+class PrettyNamedMatrix:
+    def __init__(self, matrix, colnames=None, rownames=None):
+        _colnames = list(matrix.colnames) if colnames is None else colnames
+        _rownames = list(matrix.rownames) if rownames is None else rownames
+        self._matrix = np.array(matrix).reshape(
+            (len(_rownames), len(_colnames))).transpose()
+        self._cols = _colnames
+        self._rows = _rownames
+
+    def __repr__(self):
+        return f"{self.as_list()}\n{self.get_rownames()}"
+
+    def as_list(self):
+        return {k: v for k, v in zip(self._cols, self._matrix)}
+
+    def as_dict(self):
+        return {k: {k: v for k, v in zip(self._rows, _vec)} for k, _vec in zip(self._cols, self._matrix)}
+
+    def get_rownames(self):
+        return self._rows
+
+    def get_colnames(self):
+        return self._cols
 
 
 class LM(IRegressionModel):
@@ -150,23 +232,21 @@ class LM(IRegressionModel):
 
     def __init__(self):
         self.Result = LMResult
-        self.Failed = FailedResult
         self.model_name = "lm"
-        self.coeff_matrix_index = 3
 
     def discript_model(self):
         return f"{self.model_name} model: {self.create_R_call_string()}"
 
-    def set_formula(self,y,xs):
+    def set_formula(self, y, xs):
         expression = reduce(lambda acc, e: acc+"+" +
                             e if acc != "" else e, xs, "")
         formula = f"{y}~{expression}"
         self.formula = formula
 
-    def set_fit_kwargs(self,kwargs):
+    def set_fit_kwargs(self, kwargs):
         self.call_kwargs = reduce(
             lambda acc, e: acc+","+e if acc is not "" else e,
-            [f"{key}={value}" for key, value in lm_kwargs.items()],
+            [f"{key}={value}" for key, value in kwargs.items()],
             "")
 
     def create_R_call_string(self):
@@ -177,7 +257,7 @@ class LM(IRegressionModel):
         if len(xs) is 0:
             raise Exception("At least 1 predicative variable required.")
 
-        self.set_formula(y,xs)
+        self.set_formula(y, xs)
         self.set_fit_kwargs(lm_kwargs)
 
         robjects.r.assign("d", df)
@@ -185,118 +265,61 @@ class LM(IRegressionModel):
 
         try:
             result = robjects.r(call_string)
-            self._result = self.Result(
+
+            return self.Result(
                 result, y, self.discript_model())
+
         except Exception as e:
-            self._result = self.Failed(
+
+            return self.Result(
                 None, y, self.discript_model()+f"\n{e}")
 
-        return self
 
-    def coeff(self, presenter=lambda d: d):
-        result = self.result()
-
-        if type(result) is self.Failed:
-            coeff = [[None for __ in result.variables()] for _ in range(4)]
-        else:
-            coeff_vector = result.get_coeff_matrix(self.coeff_matrix_index)
-            coeff = np.array(coeff_vector).reshape(
-                (len(result.variables(self.coeff_matrix_index)), 4)).transpose()
-
-        variables = result.variables(self.coeff_matrix_index)
-
-        coeff_array = {
-            "estimated": coeff[0],
-            "std-error": coeff[1],
-            "t-value": coeff[2],
-            "p-value": coeff[3]
-        }
-
-        coeff_dict = {
-            "estimated": {k: v for k, v in zip(variables, coeff[0])},
-            "std-error": {k: v for k, v in zip(variables, coeff[1])},
-            "t-value": {k: v for k, v in zip(variables, coeff[2])},
-            "p-value": {k: v for k, v in zip(variables, coeff[3])}
-        }
-        result_dict = {
-            "coeff": coeff_array,
-            "coeff_dict": coeff_dict,
-            "variables": result.variables()
-        }
-        return presenter(result_dict)
-
-
-class GLM(IRegressionModel):
+class GLM2(IRegressionModel):
     def __init__(self, family="gaussian", link="identity"):
-        self.Result = GLMResult
-        self.Failed = FailedResult
+        self.Result = GLM2Result
         self.distribution_family = family
         self.link_function = link
+        self.model_name = "glm2"
 
     def discript_model(self):
         return f"GLM model {self.distribution_family} family "\
             + f"{self.link_function} link: "\
             + f"{self.formula}"
 
-    def fit(self, df, y, *xs, **glm_kwargs):
-
-        if len(xs) is 0:
-            raise Exception("At least 1 predicative variable required.")
-
-        glm_kwargs_string = reduce(
-            lambda acc, e: acc+","+e if acc is not "" else e,
-            [f"{key}={value}" for key, value in glm_kwargs.items()],
-            "")
-
+    def set_formula(self, y, xs):
         expression = reduce(lambda acc, e: acc+"+" +
                             e if acc != "" else e, xs, "")
         formula = f"{y}~{expression}"
         self.formula = formula
 
-        robjects.r.assign("d", df)
+    def set_fit_kwargs(self, kwargs):
+        self.call_kwargs = reduce(
+            lambda acc, e: acc+","+e if acc is not "" else e,
+            [f"{key}={value}" for key, value in kwargs.items()],
+            "")
 
-        call_string = f"glm2({formula},family={self.distribution_family}(link={self.link_function}),data=d,{glm_kwargs_string})"
+    def create_R_call_string(self):
+        return f"{self.model_name}({self.formula},data=d,{self.call_kwargs},family={self.distribution_family}(link={self.link_function}))"
+
+    def fit(self, df, y, *xs, **glm_kwargs):
+
+        if len(xs) is 0:
+            raise Exception("At least 1 predicative variable required.")
+
+        self.set_formula(y, xs)
+        self.set_fit_kwargs(glm_kwargs)
+
+        robjects.r.assign("d", df)
+        call_string = self.create_R_call_string()
+
         try:
             result = robjects.r(call_string)
-            self._result = self.Result(
+
+            return self.Result(
                 result, y, self.discript_model())
+
         except Exception as e:
-            self._result = self.Failed(
+
+            return self.Result(
                 None, y, self.discript_model()+f"\n{e}")
-
-        return self
-
-    def coeff(self, presenter=lambda d: d):
-        result = self.result()
-
-        if type(result) is self.Failed:
-            coeff = [[None for __ in result.variables()] for _ in range(4)]
-        else:
-            coeff_vector = result.get_coeff_matrix()
-            coeff = np.array(coeff_vector).reshape(
-                (len(result.variables()), 4)).transpose()
-
-        coeff_array = {
-            "estimated": coeff[0],
-            "std-error": coeff[1],
-            "t-value": coeff[2],
-            "p-value": coeff[3]
-        }
-        coeff_dict = {
-            "estimated": {k: v for k, v in zip(result.variables(), coeff[0])},
-            "std-error": {k: v for k, v in zip(result.variables(), coeff[1])},
-            "t-value": {k: v for k, v in zip(result.variables(), coeff[2])},
-            "p-value": {k: v for k, v in zip(result.variables(), coeff[3])}
-        }
-        result_dict = {
-            "coeff": coeff_array,
-            "coeff_dict": coeff_dict,
-            "variables": result.variables()
-        }
-        return presenter(result_dict)
-
-    def AIC(self, presenter=lambda d: d):
-        if type(self.result()) is self.Failed:
-            return presenter(None)
-        aic = self.result().summary()[4][0]
-        return presenter(aic)
