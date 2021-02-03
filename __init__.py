2ea6e06af20e04a90c5b34ced2d49dedc58c9201
pegmatite <fmorisato@gmail.com>
2021-02-03 22:47:08 +0900

    Prepare package

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .fumipo_stat.test_by_r import ANOVA, shapiro_test, bartlett_test, wilcoxon_signed_rank_test
-from .fumipo_stat.r_to_py import as_dict, r_matrix_to_table
-from .fumipo_stat import presenter
-from .fumipo_stat import regression
-from .fumipo_stat.exhaustive_regression import ExhaustiveRegression
---
+++ test/connect_to_r.py
@@ -0,0 +1,3 @@
+import rpy2.robjects as robjects
+
+robjects.r('print("Hello R")')
